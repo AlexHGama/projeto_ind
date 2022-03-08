300a2983e66d310c5cea2f98490918832110055e
AlexHGama <aalexhgama@bol.com.br>
2022-03-08 13:34:48 -0300

    Projeto 1.parte finalizado....

--- asset/js/form.js
+++ asset/js/form.js
@@ -1,23 +1,41 @@
-function testacontainer_main(e) {
-    e.preventDefault();
+//function testacontainer_main(e) {
+  //  e.preventDefault();
 
-    var mercadoriaRaw = localStorage.getItem('mercadoria')
-    if (mercadoriaRaw != null) {
-        var mercadoria = JSON.parse(mercadoriaRaw)
-    }  else {
-        var mercadoria = [];
-    }
+    //var mercadoriaRaw = localStorage.getItem('mercadoria')
+    //if (mercadoriaRaw != null) {
+  //      var mercadoria = JSON.parse(mercadoriaRaw)
+   // }  else {
+   //     var mercadoria = [];
+   // }
     
-    console.log(mercadoria)
+  //  console.log(mercadoria)
+//}
 
-    
-     
+const mmascara = /[^0-9]/;
 
+function mascara(e){
+        if(mmascara.test(e.key)){
+            e.preventDefault();
+            return;
+        }
+        if(!e.target.value) return;
 
+        person = e.target.value.toString();
+        person = person.replace(/[\D]+/g, '');
+        person = person.replace(/([0-9]{1})$/g, ",$1");
 
+	if(person.length >= 6){
+		while(/([0-9]{4})[,|\.]/g.test(person)){
+			person = person.replace(/([0-9]{1})$/g, ",$1");
+			person = person.replace(/([0-9]{3})[,|\.]/g, ".$1");
+		}
+	}
 
+	e.target.value = person;
 
 }
+
+
 // limpar os dados extrato transações//
 function limpar(){
     let confir = confirm("Tem certeza que deseja apagar TODOS os dados?");
